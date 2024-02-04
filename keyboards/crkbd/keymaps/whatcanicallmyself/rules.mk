341692f05e50cce2fc899e92c4c89df40948d962
What Can I Call Myself <whatcanicallmyself@proton.me>
2024-02-04 18:18:22 +0100

    corny

---
+++ keyboards/crkbd/keymaps/whatcanicallmyself/rules.mk
@@ -0,0 +1,18 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+VIALRGB_ENABLE 		= yes
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = yes
+COMBO_ENABLE        = yes
+
+QMK_SETTINGS        = yes
+
+CONVERT_TO=promicro_rp2040
+
+WPM_ENABLE = yes
